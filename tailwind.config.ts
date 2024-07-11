eb9a9167b1b1adb17164a7fced956c9f5e8f1922
Sustainagility <132032051+jalleeeee@users.noreply.github.com>
2024-07-10 22:27:12 -0400

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import { frostedThemePlugin } from "frosted-ui";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [frostedThemePlugin({})],
+};
+export default config;
